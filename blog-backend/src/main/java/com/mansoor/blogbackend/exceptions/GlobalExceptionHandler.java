8d46cd673927016bba3dc8ab8ad5ce7d291c8d5e
Mansoor-P <munaf6007@gmail.com>
2025-02-18 14:14:22 +0530

    Implement custom exceptions for Blog service (BlogNotFoundException)

---
+++ blog-backend/src/main/java/com/mansoor/blogbackend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.mansoor.blogbackend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BlogNotFoundException.class)
+    public ResponseEntity<String> handleBlogNotFound(BlogNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
