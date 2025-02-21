e54ad715674c6f66f2825d7c767c4598d4cb284e
Mansoor-P <munaf6007@gmail.com>
2025-02-21 22:06:11 +0530

    spring security Stateless authentication

--- blog-backend/src/main/java/com/mansoor/blogbackend/config/SecurityConfig.java
+++ blog-backend/src/main/java/com/mansoor/blogbackend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -13,7 +14,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()).httpBasic(withDefaults());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
         return http.build();
     }
 }
