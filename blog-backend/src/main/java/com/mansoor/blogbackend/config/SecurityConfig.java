6f9782b29dc27272b93a29cacd3ee5fb039d32e2
Mansoor-P <munaf6007@gmail.com>
2025-02-22 17:41:07 +0530

    spring security Stateless authentication and disabling csrf with h2 database

--- blog-backend/src/main/java/com/mansoor/blogbackend/config/SecurityConfig.java
+++ blog-backend/src/main/java/com/mansoor/blogbackend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,9 +15,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) ->
+                        requests.requestMatchers("/h2-console/**").permitAll()
+                                .anyRequest().authenticated());
         http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());
+        http.headers(headers->headers.frameOptions(frameOptions->frameOptions.sameOrigin()));
+        http.csrf(csrf-> csrf.disable());
         return http.build();
     }
 }
